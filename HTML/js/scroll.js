c087608104ba93c65facdff8a4423b2fe619d9fe
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2022-01-14 14:58:37 +0300

    Implement saving preloaded state

--- HTML/js/scroll.js
+++ HTML/js/scroll.js
@@ -2,6 +2,37 @@
 const NAVBAR_THRESHOLD = 30;
 const LANDING_THRESHOLD = 0;
 
+//	Active index
+let activeIndex = -1;
+
+//	Function called to load in state from session storage
+let LoadState = () => {
+
+	//	Get list from session storage
+	activeIndex = sessionStorage.getItem('activeIndex') || -1;
+
+	//	Loop through each item
+	for (let i = 0; i <= activeIndex; i++) {
+
+		//	If landing
+		if (i == 0) {
+
+			//	Get landing section and navbar
+			let section = document.querySelector('.landing');
+			let navbar = document.querySelector('.navbar');
+			let body = document.querySelector('body');
+
+			//	Set section, body and navbar to active
+			section.classList.add('active', 'preloaded');
+			navbar.classList.add('active', 'preloaded');
+			body.classList.add('active', 'preloaded');
+
+		}
+
+	}
+	
+}
+
 //	Function called to update header state on scroll
 let UpdateHeader = () => {
 	
@@ -38,13 +69,19 @@ let CheckLanding = () => {
 	let navbar = document.querySelector('.navbar');
 	let body = document.querySelector('body');
 
-	//	Check if past landing section
-	if (scrollY >= LANDING_THRESHOLD) {
+	//	Check if past landing section and not already visible
+	if (scrollY >= LANDING_THRESHOLD && activeIndex < 0) {
 
 		//	Set section, body and navbar to active
 		section.classList.add('active');
 		navbar.classList.add('active');
 		setTimeout(() => body.classList.add('active'), 5500);
+
+		//	Update active index
+		activeIndex = 0;
+
+		//	Save to session storage
+		sessionStorage.setItem('activeIndex', activeIndex);
 
 	}
 
@@ -52,4 +89,5 @@ let CheckLanding = () => {
 
 //	Subscribe to event listeners
 document.addEventListener('scroll', UpdateHeader);
+document.addEventListener('DOMContentLoaded', LoadState);
 document.addEventListener('DOMContentLoaded', CheckLanding);
