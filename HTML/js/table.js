da79e57cd90c324871f95f97fa9868bcf9e7fa8a
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2022-01-16 10:22:18 +0300

    Implement menu actions

--- HTML/js/table.js
+++ HTML/js/table.js
@@ -1,5 +1,17 @@
+//	Counter
+let count = 0;
+
+//	List of entries
+let entries = [];
+
+//	List of selected entries
+let selected = [];
+
 //	Function called when add new task button is clicked
 let AddTableEntry = ({ title, deadline, priority }) => {
+
+	//	Increment count
+	count++;
 
 	//	Sanitise inputs
 	title = title.replace('<', '').replace('>', '');
@@ -8,27 +20,18 @@ let AddTableEntry = ({ title, deadline, priority }) => {
 
 	//	Create a new container element
 	let row = document.createElement('tr');
-	row.className = 'row';
+	row.className = 'active row entry';
+	row.id = `row_${count}`;
 
 	//	Generate row html
 	let html = `
-		<tr class='row'>
-
-			<td class='tiny'>
-				
-				<div class='checkbox'><label><input type='checkbox'/><i class='fa fa-check'></i></label></div>
-
-			</td>
-
-			<td class='big'>${title}</td>
-
-			<td class='small'>${deadline}</td>
-
-			<td class='small'>${priority}</td>
-
-			<td class='tiny'><i class='button fa fa-ellipsis-v'></i></td>
-
-		</tr>
+		<td class='tiny'>
+			<div class='checkbox'><label><input type='checkbox'/><i class='fa fa-check'></i></label></div>
+		</td>
+		<td class='big'>${title}</td>
+		<td class='small'>${deadline}</td>
+		<td class='small'>${priority}</td>
+		<td class='tiny actions' id='actions_${count}'><i onclick='ClickActionsButton(${count})' class='button fa fa-ellipsis-v'></i></td>
 	`;
 
 	//	Add html to row element
@@ -39,8 +42,117 @@ let AddTableEntry = ({ title, deadline, priority }) => {
 
 }
 
+//	Function called to remove an entry
+let RemoveTableEntry = (index) => {
+
+	//	Find entry
+	let entry = document.getElementById(`row_${index}`);
+
+	//	Extract data from entry
+	let data = {
+		title		: entry.children[1].innerHTML.replace('<', '').replace('>', ''),
+		deadline	: entry.children[2].innerHTML.replace('<', '').replace('>', ''),
+		priority	: entry.children[3].innerHTML.replace('<', '').replace('>', ''),
+	};
+
+	//	Remove from list of entries
+	for (let i = 0; i < entries.length; i++) {
+
+		//	Get element at this index
+		let elem = entries[i];
+
+		//	If this element is getting removed
+		if (elem.title == data.title && elem.deadline == data.deadline && elem.priority == data.priority) {
+
+			//	Delete this entry
+			entries.splice(i, 1);
+
+			//	Delete element
+			setTimeout(() => entry.remove(), 150);
+
+		}
+	
+	}
+
+	//	Return the element being deleted
+	return entry;
+
+}
+
+//	Function called when the actions button is clicked
+let ClickActionsButton = (index) => {
+
+	//	If clicking on table menu button but there are no entries selected
+	if (index === -1 && selected.length === 0) return;
+
+	//	Find actions button
+	let button = document.getElementById(`actions_${index}`);
+
+	//	Create a new container element
+	let menu = document.createElement('div');
+	menu.className = 'menu';
+
+	//	Generate menu html
+	let html = `
+		<button onclick='CompleteTask(${index})'>Complete</button>
+		<button onclick='RemoveTask(${index})'>Delete</button>
+	`;
+
+	//	Add html to menu element
+	menu.innerHTML = html;
+
+	//	Add to actions button
+	button.appendChild(menu);
+
+	//	Play animation
+	document.querySelector('.menu').classList.add('active');
+
+	//	Add event listener
+	setTimeout(() => document.addEventListener('click', CheckMenuClick), 10);
+
+}
+
+//	Function called to check if user clicks outside of menu
+let CheckMenuClick = (e) => {
+	
+	//	Get menu
+	let menu = document.querySelector('.menu');
+
+	//	Delete menu element
+	menu.remove();
+
+	//	Unsubscribe from event
+	document.removeEventListener('click', CheckMenuClick);
+
+}
+
+//	Function called to complete task
+let CompleteTask = (index) => {
+
+	//	Delete entry and get element being deleted
+	let entry = RemoveTableEntry(index);
+
+	//	Play animation
+	entry.classList.replace('active', 'complete');
+
+}
+
+//	Function called to remove task
+let RemoveTask = (index) => {
+	
+	//	Delete entry and get element being deleted
+	let entry = RemoveTableEntry(index);
+
+	//	Play animation
+	entry.classList.remove('active');
+
+}
+
 //	Function called when add new task button is clicked
 let ClickAddEntry = () => {
+
+	//	If already adding then exit
+	if (document.getElementById('add_entry')) return;
 
 	//	Create a new container element
 	let row = document.createElement('tr');
@@ -68,13 +180,22 @@ let ClickAddEntry = () => {
 	//	Add to table body
 	document.querySelector('.finished_product .list .table .rows').appendChild(row);
 
+	//	Play animation
+	setTimeout(() => document.getElementById('add_entry').classList.add('active'), 150);
+
 }
 
 //	Function called to cancel add entry
 let CancelAddEntry = () => {
 
+	//	Get entry
+	let entry = document.getElementById('add_entry');
+
+	//	Play animation
+	entry.classList.remove('active');
+
 	//	Delete add entry
-	document.getElementById('add_entry').remove();
+	setTimeout(() => entry.remove(), 150);
 
 }
 
@@ -82,20 +203,23 @@ let CancelAddEntry = () => {
 let ConfirmAddEntry = () => {
 
 	//	Get add entry element
-	let element = document.getElementById('add_entry');
+	let entry = document.getElementById('add_entry');
 
 	//	Get title, deadline and priority
-	let title = element.children[1].children[0].value;
-	let deadline = element.children[2].children[0].value;
-	let priority = element.children[3].children[0].value;
+	let title = entry.children[1].children[0].value;
+	let deadline = entry.children[2].children[0].value;
+	let priority = entry.children[3].children[0].value;
 
 	//	Exit if there are any empty inputs
 	if (!title || !deadline || !priority) return;
+
+	//	Add to list of entries
+	entries.push({ title, deadline, priority });
 
 	//	Create a new entry
 	AddTableEntry({ title, deadline, priority });
 
 	//	Delete add entry
-	document.getElementById('add_entry').remove();
+	entry.remove();
 
 }
