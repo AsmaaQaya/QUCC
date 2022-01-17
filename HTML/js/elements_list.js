a605fa9b028d84c0b45ca8db5bb0256ea6e30d48
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2022-01-17 16:09:43 +0300

    Implement carousel functionality

---
+++ HTML/js/elements_list.js
@@ -0,0 +1,15 @@
+//	Hardcoded list of elements
+let elements = [
+	{
+		name		: '&lt;div&gt;',
+		description	: 'used as a container for other elements, defines a division or section in an <br/>HTML document. Also commonly used as a background or for geometric <br/>shapes when styled correctly with CSS.',
+		input		: '&lt;div style="background-color: black"&gt;<br/>&lt;/div&gt;',
+		output		: '<div style="background-color: black; width: 100px; height: 100px"></div>'
+	},
+	{
+		name		: '&lt;h1&gt;',
+		description	: 'used as a container for other elements, defines a division or section in an <br/>HTML document. Also commonly used as a background or for geometric <br/>shapes when styled correctly with CSS.',
+		input		: '&lt;h1&gt;Header 1&lt;/h1&gt;',
+		output		: '<h1>Header 1</h1>'
+	},
+];
